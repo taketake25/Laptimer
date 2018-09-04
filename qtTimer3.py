a1b3766735bb2b8c3249727ea41936f1277ff388
taketake25 <takeru31415@gmail.com>
2018-09-05 02:02:22 +0900

    programing progress bar that showes time limit (3 minutes)

--- qtTimer3.py
+++ qtTimer3.py
@@ -5,12 +5,13 @@ import sys
 import time
 from PyQt5 import Qt
 from PyQt5.QtCore import QTimer ,QTime
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.uic import loadUi
 import threading
 import csv
 #i2c=smbus.SMBus(1)
 
-TICK_TIME=30
+TICK_TIME=10
 
 class StopWatch(Qt.QMainWindow):
     def __init__(self,parent=None):
@@ -24,11 +25,12 @@ class StopWatch(Qt.QMainWindow):
         self.driving=False #走行中であるか
         self.afterSence=False #センサーが反応してすぐか
         self.afterSenceCount=0
+        self.sanpunStart=0
 
-        self.rf=open(sys.argv[1],"r",encoding="utf-8") #csvファイルの読み込み
+
         filename="./out/%s%d.csv"%(sys.argv[2],int(time.time())%100000)
+        self.rf=open(sys.argv[1],"r",encoding="utf-8") #csvファイルの読み込み
         self.wf=open(filename,"w", newline='')
-
         self.reader=csv.reader(self.rf, delimiter=",") #リスト形式
         self.writer=csv.writer(self.wf, delimiter=",") #リスト形式
         self.header=next(self.rf) #最初の行を得る
@@ -38,35 +40,49 @@ class StopWatch(Qt.QMainWindow):
         self.start.clicked.connect(self.do_start)
         self.save.clicked.connect(self.do_save)
 
+        # self.setAutoFillBackground(True)
+        # p = self.palette()
+        # p.setColor(self.backgroundRole(), QColor(255,255,255))
+        # self.setPalette(p)
+
         self.timer=Qt.QTimer() #アニメーション(7セグ表示部分)用
         self.timer.setInterval(TICK_TIME)
         self.timer.timeout.connect(self.tick)
         self.timer.start()
+        self.key.deselect()
+        self.group.deselect()
+        self.name.deselect()
+        self.machineName.deselect()
+        self.timeLimit.setValue(0)
 
         # csvファイルの1行1列の数字にゴミが入るので、回避代わりに(誰かデバッグして)
-        self.do_reset()
+        self.do_reset(False)
+
+    def __del__(self,parent=None):
+        self.do_reset(False)
+        self.rf.close()
+        self.wf.close()
+    
 
     def keyPressEvent(self,event):
         if event.key() == Qt.Qt.Key_Escape:
-            # self.rf.close()
-            # self.wf.close()
             self.close()
         elif event.key() == Qt.Qt.Key_R:
-            self.do_reset()
+            self.do_reset(False)
         elif event.key() == Qt.Qt.Key_P:
             self.do_pause()
-        elif event.key() == Qt.Qt.Key_S:
-            self.do_save()
+        elif event.key() == Qt.Qt.Key_N:
+            self.do_reset(True)
         elif event.key() == Qt.Qt.Key_T:
             self.getFromNucleo=0;
 
     def display(self):
         now=time.time()
+        self.timeLimit.setValue((now-self.sanpunStart)/1.8)
+
         if not self.driving:
             now=self.finishTime
-
         self.runTime=now-self.startTime
-        # print("%6.2f, %6.2f, %6.2f, %6.2f"%(self.startTime,self.finishTime,now,self.runTime))
         self.currentTime.display("%d:%05.2f" %(self.runTime // 60, self.runTime % 60))
 
     def tick(self):
@@ -101,7 +117,10 @@ class StopWatch(Qt.QMainWindow):
 
         self.display()
 
-    def do_start(self): #
+    def do_start(self):
+        if self.sanpunStart==0:
+            self.sanpunStart=time.time()
+
         if self.lapNum<=3:
             self.startTime=time.time()
             self.driving=True
@@ -130,9 +149,9 @@ class StopWatch(Qt.QMainWindow):
         self.start.clicked.connect(self.do_start)
         self.lapNum+=1
     
-    def do_reset(self):
+    def do_reset(self,noResult):
         if not self.lapNum==0:
-            self.do_save()
+            self.do_save(noResult)
         self.driving=False
         self.runTime=0
         self.startTime=0
@@ -150,20 +169,26 @@ class StopWatch(Qt.QMainWindow):
         self.firstTimeVal.setText("0:00.00")
         self.secondTimeVal.setText("0:00.00")
         self.lapNum=1
-        self.lapTime=[0,0,0]
+        self.lapTime=[1000,1000,1000]
+        self.sanpunStart=0
+        self.timeLimit.setValue(0)
 
-    def do_save(self): #結果をcsvファイルに書き込み
-        print(self.lapTime)
+    def do_save(self,noResult): #結果をcsvファイルに書き込み
+        t=self.firstTimeVal.text()
+        if t[0]=='n':
+            self.lapTime[0]=1000.0
+        t=self.secondTimeVal.text()
+        if t[0]=='n':
+            self.lapTime[1]=1000.0
+        if noResult:
+            self.lapTime[2]=1000.0
+
         for i in range(3):
-            words=self.header.split(',')
-            writeWord="%s,%s,%s,%s,%d,%6.2f"%(int(words[0]),str(words[1]),str(words[2]),str(words[3]),1,self.lapTime[i])
+            print("%5.2f, "%(self.lapTime[i]) ,end="")
+            writeWord="%s,%s,%s,%s,%d,%6.2f"%(self.key.text(),self.group.text(),self.name.text(),self.machineName.text(),i+1,self.lapTime[i])
             self.writer.writerow(writeWord.split(','))
+        print("\n")
 
-    def close(self):
-        self.do_reset()
-        self.rf.close()
-        self.wf.close()
-        super.close()
 
 if __name__ == '__main__':
     app=Qt.QApplication(sys.argv)
